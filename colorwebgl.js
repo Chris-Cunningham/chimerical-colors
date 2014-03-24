f29019f7787812ab1cc17dfd45f4f0115a9eb136
Chris Cunningham <fivecs@gmail.com>
2014-03-24 14:40:05 -0500

    Adding on the prism (non-RGB-coordinates)

--- colorwebgl.js
+++ colorwebgl.js
@@ -1,4 +1,3 @@
-
 function initWebGL() {
 
     var gl; // A global variable for the WebGL context
@@ -122,8 +121,13 @@ var smallcubeVertexPositionBuffer;
 var smallcubeVertexColorBuffer;
 var smallcubeVertexIndexBuffer;
 
+var prismVertexPositionBuffer;
+var prismVertexColorBuffer;
+var prismVertexIndexBuffer;
+
 var rSquare = 0;
 var rCube = 0;
+var rPrism = 0;
 
 function initBuffers() {
 
@@ -282,10 +286,10 @@ function initBuffers() {
     cubeVertexColorBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, cubeVertexColorBuffer);
 
-    // Color everything based on its RGB location.
-    unpackedColors = colorVerticesRGB(vertices);
+    // Color everything based on its RGB location. Warning: We will use cubeColors again for the prism.
+    cubeColors = colorVerticesRGB(vertices);
 
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(unpackedColors), gl.STATIC_DRAW);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(cubeColors), gl.STATIC_DRAW);
     cubeVertexColorBuffer.itemSize = 4;
     cubeVertexColorBuffer.numItems = 24;
 
@@ -302,6 +306,74 @@ function initBuffers() {
     gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(cubeVertexIndices), gl.STATIC_DRAW);
     cubeVertexIndexBuffer.itemSize = 1;
     cubeVertexIndexBuffer.numItems = 36;
+
+
+    prismVertexPositionBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, prismVertexPositionBuffer);
+    vertices = [
+      // Front face
+       0.0, -1.0, -1/3,
+       1.0, -1/2,  1/3,
+       0.0,  0.0,  1.0,
+      -1.0, -1/2,  1/3,
+
+      // Back face
+       0.0,  0.0, -1.0,
+      -1.0,  1/2, -1/3,
+       0.0,  1.0,  1/3,
+       1.0,  1/2, -1/3,
+
+      // Top face
+      -1.0,  1/2, -1/3,
+      -1.0, -1/2,  1/3,
+       0.0,  0.0,  1.0,
+       0.0,  1.0,  1/3,
+
+      // Bottom face
+       0.0,  0.0, -1.0,
+       1.0,  1/2, -1/3,
+       1.0, -1/2,  1/3,
+       0.0, -1.0, -1/3,
+
+      // Right face
+       1.0,  1/2, -1/3,
+       0.0,  1.0,  1/3,
+       0.0,  0.0,  1.0,
+       1.0, -1/2,  1/3,
+
+      // Left face
+       0.0,  0.0, -1.0,
+       0.0, -1.0, -1/3,
+      -1.0, -1/2,  1/3,
+      -1.0,  1/2, -1/3,
+    ];
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+    prismVertexPositionBuffer.itemSize = 3;
+    prismVertexPositionBuffer.numItems = 24;
+
+    prismVertexColorBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, prismVertexColorBuffer);
+
+    // Don't color everything based on its RGB location. Use the previous color setup from the cube.
+    // cubeColors = colorVerticesRGB(vertices);
+
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(cubeColors), gl.STATIC_DRAW);
+    prismVertexColorBuffer.itemSize = 4;
+    prismVertexColorBuffer.numItems = 24;
+
+    prismVertexIndexBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, prismVertexIndexBuffer);
+    var prismVertexIndices = [
+      0, 1, 2,      0, 2, 3,    // Front face
+      4, 5, 6,      4, 6, 7,    // Back face
+      8, 9, 10,     8, 10, 11,  // Top face
+      12, 13, 14,   12, 14, 15, // Bottom face
+      16, 17, 18,   16, 18, 19, // Right face
+      20, 21, 22,   20, 22, 23  // Left face
+    ]
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(prismVertexIndices), gl.STATIC_DRAW);
+    prismVertexIndexBuffer.itemSize = 1;
+    prismVertexIndexBuffer.numItems = 36;
 
 }
 
@@ -477,6 +549,18 @@ function drawScene() {
     mvPopMatrix();
 
 
+    mvPushMatrix();
+    mat4.rotate(mvMatrix, degToRad(rPrism), [1, 0, 0]);
+    mat4.translate(mvMatrix, [3.0, 0.0, 0.0]);
+    gl.bindBuffer(gl.ARRAY_BUFFER, prismVertexPositionBuffer);
+    gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, prismVertexPositionBuffer.itemSize, gl.FLOAT, false, 0, 0);
+    gl.bindBuffer(gl.ARRAY_BUFFER, prismVertexColorBuffer);
+    gl.vertexAttribPointer(shaderProgram.vertexColorAttribute, prismVertexColorBuffer.itemSize, gl.FLOAT, false, 0, 0);
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, prismVertexIndexBuffer);
+    setMatrixUniforms();
+    gl.drawElements(gl.TRIANGLES, prismVertexIndexBuffer.numItems, gl.UNSIGNED_SHORT, 0);
+    mvPopMatrix();
+
 }
 
 var lastTime = 0;
@@ -487,6 +571,7 @@ function animate() {
 
       rSquare -= (40 * elapsed) / 1000.0;
       rCube -= (40 * elapsed) / 1000.0;
+      rPrism -= (40 * elapsed) / 1000.0;
     }
     lastTime = timeNow;
  }
